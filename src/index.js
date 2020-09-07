c1d3c02bc8dcccc4c5ba561ccc8bf05a56a50f70
Janko <mailodjanka@gmail.com>
2020-09-07 11:07:56 +0200

    Imports createStore from redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { createStore } from 'redux'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore()
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
