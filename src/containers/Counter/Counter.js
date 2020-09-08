1031f55b8379fa531f04b32386e8900b83c7b39f
Janko <mailodjanka@gmail.com>
2020-09-08 12:26:24 +0200

    Changes Counter to a functional component as it no longer needs to hold state

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,53 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
-class Counter extends Component {
-  state = {
-    counter: 0,
-  };
-
-  counterChangedHandler = (action, value) => {
-    switch (action) {
-      case "inc":
-        this.setState((prevState) => {
-          return { counter: prevState.counter + 1 };
-        });
-        break;
-      case "dec":
-        this.setState((prevState) => {
-          return { counter: prevState.counter - 1 };
-        });
-        break;
-      case "add":
-        this.setState((prevState) => {
-          return { counter: prevState.counter + value };
-        });
-        break;
-      case "sub":
-        this.setState((prevState) => {
-          return { counter: prevState.counter - value };
-        });
-        break;
-    }
-  };
-
-  render() {
-    return (
-      <div>
-        <CounterOutput value={this.props.cntr} />
-        <CounterControl label="Increment" clicked={this.props.increment} />
-        <CounterControl label="Decrement" clicked={this.props.decrement} />
-        <CounterControl label="Add 5" clicked={this.props.addValue} />
-        <CounterControl
-          label="Subtract 5"
-          clicked={this.props.substractValue}
-        />
-      </div>
-    );
-  }
-}
+const Counter = (props) => {
+  return (
+    <div>
+      <CounterOutput value={props.cntr} />
+      <CounterControl label="Increment" clicked={props.increment} />
+      <CounterControl label="Decrement" clicked={props.decrement} />
+      <CounterControl label="Add 5" clicked={props.addValue} />
+      <CounterControl label="Subtract 5" clicked={props.substractValue} />
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
