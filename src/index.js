e429d53d36f18913acd9fe9b3a6ff07a307f1bca
Janko <mailodjanka@gmail.com>
2020-09-29 14:43:11 +0200

    Uses applyMiddleware from redux and uses it to console log action and next state, to see it in action

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import counterReducer from "./store/reducers/counter"
 import resultsReducer from "./store/reducers/results"
@@ -13,7 +13,17 @@ const rootReducer = combineReducers({
   results: resultsReducer
 })
 
-const store = createStore(rootReducer)
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware], Dispatching', action)
+      const result = next(action)
+      console.log('[Middleware], next state', store.getState())
+      return result
+    }
+  }
+}
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
