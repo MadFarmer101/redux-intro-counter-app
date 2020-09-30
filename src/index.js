dcfcc2cfeba74d319cc6142ebbfea22e28dd59c0
Janko <mailodjanka@gmail.com>
2020-09-30 11:21:40 +0200

    Imports thunk and calls on it when applying middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import thunk from "redux-thunk"
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import counterReducer from "./store/reducers/counter"
@@ -26,7 +27,7 @@ const logger = store => {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(logger)
+    applyMiddleware(logger, thunk)
   ));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
