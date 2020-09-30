89d23bbac2e461577a185062f181487060d6edb5
Janko <mailodjanka@gmail.com>
2020-09-30 11:28:13 +0200

    Sets a timeout when of 2 sec when storing a result to see thunk in action. that's how async works with redux

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -31,10 +31,18 @@ export const substractValue = (value) => {
   };
 };
 
-export const storeResult = (result) => {
+export const saveResult = (result) => {
   return {
     type: STORE_RESULT,
     payload: result,
+  };
+};
+
+export const storeResult = (result) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(saveResult(result));
+    }, 2000);
   };
 };
 
